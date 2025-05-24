07a7ba6b709ec85d2728f22770277a14d2da3c1d
Tawfiq Alamarneh <amarneh1@hotmail.com>
2025-05-25 01:19:02 +0400

    End Of Stage 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,12 +13,15 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+//app.UseCors(x=>x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthorization();
 
